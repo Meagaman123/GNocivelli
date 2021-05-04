7d4afdee93e3f5155d4989eaedfd47a266a6e5be
student <giannoci@gmail.com>
2021-05-04 14:01:31 +0100

    Created Rails App and Configured Boostrap

--- assignment2/reviewapp/reviewapp/asgi.py
+++ assignment2/reviewapp/reviewapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for reviewApp project.
+ASGI config for reviewapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reviewApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reviewapp.settings')
 
 application = get_asgi_application()
