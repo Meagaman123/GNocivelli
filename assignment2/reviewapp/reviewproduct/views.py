490658ecbadc88283170fcc0fe1519b304527ab1
student <giannoci@gmail.com>
2021-05-04 17:51:31 +0100

    added template inherritance in preperation for the css and the implimentation of a navigation section

--- assignment2/reviewapp/reviewproduct/views.py
+++ assignment2/reviewapp/reviewproduct/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'reviewproduct/home.html')
+    return render(request, 'reviewproduct/home.html', {'title': 'Home'})
 def about (request):
-    return render(request, 'reviewproduct/about.html')
+    return render(request, 'reviewproduct/about.html', {'title': 'About Us'})
 def contact (request):
-        return render(request, 'reviewproduct/contact.html')
+        return render(request, 'reviewproduct/contact.html', {'title': 'Contact'})
 def products (request):
-        return render(request, 'reviewproduct/products.html')
+        return render(request, 'reviewproduct/products.html', {'title': 'Procusts'})
+
+
